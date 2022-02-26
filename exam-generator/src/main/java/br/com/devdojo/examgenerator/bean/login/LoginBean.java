6b87398e7c6e8f3c48667327809f0beac7aa8c4f
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-02-26 10:58:55 -0300

    Java

--- exam-generator/src/main/java/br/com/devdojo/examgenerator/bean/login/LoginBean.java
+++ exam-generator/src/main/java/br/com/devdojo/examgenerator/bean/login/LoginBean.java
@@ -1,20 +1,30 @@
 package br.com.devdojo.examgenerator.bean.login;
 
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.devdojo.examgenerator.persistence.dao.LoginDAO;
+import br.com.devdojo.examgenerator.persistence.model.Token;
 
 @Named
 @ViewScoped
 public class LoginBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String username;
 	private String password;
 	private final LoginDAO loginDAO;
 	private final ExternalContext externalContext;
+	
+	@Inject
 	public LoginBean(String username, String password, LoginDAO loginDAO, ExternalContext externalContext) {
 		super();
 		this.username = username;
@@ -23,5 +33,31 @@ public class LoginBean implements Serializable {
 		this.externalContext = externalContext;
 	}
 	
+	public String login() throws UnsupportedEncodingException {
+		Token token = loginDAO.loginReturningToken("Caio", "password");
+		return token == null ? null : addTokenAndExpirationTimeToCookiesAndReturnIndex(token);
+		
+	}
+	private String addTokenAndExpirationTimeToCookiesAndReturnIndex(Token token) throws UnsupportedEncodingException {
+		externalContext.addResponseCookie("token", URLEncoder.encode(token.getToken(),"UTF-8"),null);
+		externalContext.addResponseCookie("expirationTime", token.getExpirationTime().toString(), null);
+		return "index.xhtml?faces-redirect=true";
+	}
 	
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
---
+++ exam-generator/src/main/java/br/com/devdojo/examgenerator/filte/LoginFilter.java
@@ -0,0 +1,27 @@
+package br.com.devdojo.examgenerator.filte;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns = {"/*"}, description = "Session checker filter")
+public class LoginFilter implements Filter, Serializable {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+	}
+
+}
